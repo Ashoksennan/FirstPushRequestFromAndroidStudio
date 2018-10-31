7e5e4571eca1efc04745be51f0a1cf6262c52059
Ashok Sennan Admin <ashoksennan426@gmail.com>
2018-10-31 18:38:07 +0530

    this module has login functionalities

--- app/src/main/java/com/example/admin/gitapplicationdemo/MainActivity.java
+++ app/src/main/java/com/example/admin/gitapplicationdemo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "hi new changes to this file", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hi changes to this file", Toast.LENGTH_SHORT).show();
     }
 }
